0583f6006af4b52f6aa0fed721828db3239ac911
Ardduli <ylimartimo.arttu@gmail.com>
2023-02-28 16:27:09 +0200

    created basic js functions

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+// add function
+function add(a, b){
+    return a + b;
+}
 
+// substract function
+function subtract(a, b){
+    return a - b;
+}
+
+// multiply function
+function multiply(a, b){
+    return a * b;
+}
+
+//divide function
+function divide(a, b){
+    return a / b;
+}
+
+function operate(func, a, b){
+    return func(a, b);
+}
