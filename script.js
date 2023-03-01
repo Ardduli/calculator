0227fdab6de8503ee6df40d77e273a202bdf1b66
Ardduli <ylimartimo.arttu@gmail.com>
2023-03-01 12:25:01 +0200

    added functionality to the number and reset buttons

--- script.js
+++ script.js
@@ -21,3 +21,34 @@ function divide(a, b){
 function operate(func, a, b){
     return func(a, b);
 }
+
+// adds numbers to the display
+
+const numberButtons = document.querySelectorAll('.number');
+let output = document.querySelector('output')
+
+output.textContent = 0;
+
+numberButtons.forEach(button => {
+    button.addEventListener('click',() =>{
+        const value = button.value
+            if(output.textContent == 0){
+                output.textContent = value;
+            } else {
+                output.textContent += value;
+            }
+    })
+});
+
+// resets the display
+
+const resetButton = document.getElementById('reset');
+    resetButton.addEventListener('click', () => output.textContent = 0);
+
+const clearButton = document.getElementById('clear');
+    clearButton.addEventListener('click', () => {
+        const valueArray = output.textContent.split('');
+        const newArray = valueArray.pop();
+        const newValue = valueArray.toString().replace(/[^A-Za-z0-9]/g, "");
+        output.textContent = newValue;
+    });
